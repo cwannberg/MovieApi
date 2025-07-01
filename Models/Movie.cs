b21b37f57c715dfbe826422f0eaa6a40a6293152
Cilla <ceciliawannberg@gmail.com>
2025-07-01 09:20:19 +0200

    Add project files.

---
+++ Models/Movie.cs
@@ -0,0 +1,11 @@
+﻿namespace MovieApi.Models
+{
+    public class Movie
+    {
+        private int Id { get; set; }
+        private string Title { get; set; }
+        private int Year { get; set; }
+        private string Genre { get; set; }
+        private double Duration { get; set; }
+    }
+}
